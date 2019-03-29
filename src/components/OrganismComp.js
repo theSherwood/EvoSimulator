b906f139c7daa13907cdbc16bce903ce310038de
admls <adam.the.sherwood@gmail.com>
2019-03-29 17:57:37 +0000

    renames component to avoid confusion with data model

---
+++ src/components/OrganismComp.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import uuid from "uuid";
+
+class OrganismComp extends Component {
+  constructor(props) {
+    super(props);
+
+    // this.state = {
+    //   parent: props.parent,
+    //   children: [],
+    //   age: 0,
+    //   isAlive: true,
+    //   time: props.time,
+    //   uuid: uuid()
+    // }
+  }
+
+  render() {
+    return <div />;
+  }
+}
+
+export default OrganismComp;
