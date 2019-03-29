bf2989b4e31f20b56e4dcb62205815fc398f6cea
admls <adam.the.sherwood@gmail.com>
2019-03-29 18:27:23 +0000

    unrollSection helper function

--- src/helpers/dataHelpers.js
+++ src/helpers/dataHelpers.js
@@ -3,3 +3,11 @@ export function randomInt(min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
 }
+
+export function unrollSection(landscape, xStart, xEnd, yStart, yEnd) {
+  let section = [];
+  for (let i = yStart; i < yEnd + 1; i++) {
+    section = section.concat(landscape[i].slice(xStart, xEnd + 1));
+  }
+  return section;
+}
