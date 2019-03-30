7ff081542ec39d3cffffcc1471383b316df42282
admls <adam.the.sherwood@gmail.com>
2019-03-30 01:41:38 +0000

    slight cleanup and reformat

--- src/components/Organism.js
+++ src/components/Organism.js
@@ -2,31 +2,30 @@ import React, { Component, Fragment } from "react";
 import Cell from "./Cell";
 
 class Organism extends Component {
-  constructor(props) {
-    super(props);
-
-    // this.state = {
-    //   parent: props.parent,
-    //   children: [],
-    //   age: 0,
-    //   isAlive: true,
-    //   time: props.time,
-    //   uuid: uuid()
-    // }
-  }
-
   render() {
-    const { cellHeight, cellWidth } = this.props;
+    const { cellHeight, cellWidth, x, y, genome, uuid } = this.props;
     return (
       <Fragment>
-        <Cell
-          key={"blah"}
-          y={`${cellHeight}%`}
-          x={`${cellWidth}%`}
-          height={`${cellHeight}%`}
-          width={`${cellWidth}%`}
-          fill={`rgba(${255},${0},${0},1)`}
+        <rect
+          y={`${y[0] * cellHeight}%`}
+          x={`${x[0] * cellWidth}%`}
+          height={`${(y[1] - y[0] + 1) * cellHeight}%`}
+          width={`${(x[1] - x[0] + 1) * cellWidth}%`}
+          strokeWidth="2"
+          stroke="rgba(255,255,255,1)"
         />
+        {genome.map((row, i) =>
+          row.map((cell, j) => (
+            <rect
+              key={`${i},${j}`}
+              y={`${(y[0] + i) * cellHeight}%`}
+              x={`${(x[0] + j) * cellWidth}%`}
+              height={`${cellHeight}%`}
+              width={`${cellWidth}%`}
+              fill={`rgba(${cell * 25},${255 - cell * 25},${0},1)`}
+            />
+          ))
+        )}
       </Fragment>
     );
   }
